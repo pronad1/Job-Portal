fca8f62a3d534ab71956ac5377fcd9695ab668d8
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-07 18:51:15 +0600

    2/7/25

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
+    @vite(['resources/css/app.css'])
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
